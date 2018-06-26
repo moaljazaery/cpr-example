9adaa6e47fe3f68f19f7607dfd2d2375205b9e4b
moaljazaery <mhd.jazaery@gmail.com>
2018-06-25 22:08:16 -0700

    update example

--- example.cpp
+++ example.cpp
@@ -3,9 +3,13 @@
 #include <cpr/cpr.h>
 #include <json.hpp>
 
-
 int main(int argc, char** argv) {
-    auto response = cpr::Get(cpr::Url{"https://httpbin.org/get"});
-    auto json = nlohmann::json::parse(response.text);
-    std::cout << json.dump(4) << std::endl;
+    std::string key = argv[1];
+    auto r = cpr::Get(cpr::Url{"http://ec2-34-215-218-52.us-west-2.compute.amazonaws.com/license/api/v1/license/"+key});
+    if (r.status_code == 200)
+        std::cout << "Valid" << std::endl;
+    else
+        std::cout << "Not Valid" << std::endl;
 }
+
+
